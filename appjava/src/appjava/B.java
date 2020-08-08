953de1d8f9477266cd07de109ececf7dd2281ac1
DuyDung <dinhvandung791@gmail.com>
2020-08-08 11:01:09 +0700

    test abstract and interface

---
+++ appjava/src/appjava/B.java
@@ -0,0 +1,33 @@
+package appjava;
+
+import appjava.interfaces.A;
+
+abstract class B implements A {
+	
+	@Override
+	public void c() {
+		 System.out.println("I am C");	
+	}
+}
+
+class M extends B{
+
+	@Override
+	public void a() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void b() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void d() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
---
+++ appjava/src/appjava/interfaces/A.java
@@ -0,0 +1,8 @@
+package appjava.interfaces;
+
+public interface A {
+	 	void a();
+	    abstract void b();
+	    public void c();
+	    public abstract void d();
+}
